bb77d601c8f57d2fba31f6e5e3968c88da79b205
cristhianGigpz <cjacevedot@gmail.com>
2025-12-18 12:02:11 -0500

    asistente legal finalizado

--- fastapi_app_stream.py
+++ fastapi_app_stream.py
@@ -26,9 +26,29 @@ class ChatRequest(BaseModel):
 
 async def stream_chat_response(history: list):
     async with client.responses.stream(
-        model="gpt-4o",
+        model="gpt-4.1",
+        prompt={
+            "id": "pmpt_69039a0eca4c8193b8387ef187112b3c0b7c145f0376397d",
+            "version": "2"
+        },
         input=history,
+        text={
+            "format": {
+            "type": "text"
+            }
+        },
+        reasoning={},
+        tools=[
+            {
+            "type": "file_search",
+            "vector_store_ids": [
+                "vs_69039889c3e48191aa4ea7cba52f1d06"
+            ]
+            }
+        ],
         max_output_tokens=2048,
+        store=True,
+        include=["web_search_call.action.sources"]
     ) as stream:
         async for event in stream:
             if event.type == "response.output_text.delta":
@@ -41,12 +61,13 @@ async def chat_endpoint(req: ChatRequest):
     user_msg = req.message.strip()
 
     history = req.history or [
-        {"role": "system", "content": "Eres un asistente útil que habla en español."},
+        {"role": "system", "content": "Eres un asistente legal especializado en derechos constitucionales. Basa tu respuestas ÚNICAMENTE en el documento adjunto \"constitución política del Perú\" y documentos adjuntos. Si la información no esta disponible en los documentos, responde que no se encuentra disponible."},
     ]
 
     if not user_msg:
         return {"error": "El mensaje no puede estar vacío."}
     
+    history.append({"role": "user", "content": "muestra el articulo o ley donde especifica lo consultado"})
     history.append({"role": "user", "content": user_msg})
 
     return StreamingResponse(
